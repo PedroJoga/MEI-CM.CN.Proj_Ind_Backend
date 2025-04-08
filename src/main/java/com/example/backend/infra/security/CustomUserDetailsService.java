9b477ef8a6c75f1702af4142c2c77e3936f6ea92
PedroJoga <97087028+PedroJoga@users.noreply.github.com>
2025-04-08 18:05:27 +0100

    initial implementation of user authentication

---
+++ src/main/java/com/example/backend/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.example.backend.infra.security;
+
+import com.example.backend.domain.user.User;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
